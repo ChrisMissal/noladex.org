4284d22afdb65365e206a9932b0639578d42aa2f
Scott Block <scott@scottblock.com>
2012-03-30 14:18:36 -0400

    s3 stuff

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   AWS::S3::Base.establish_connection!(
-    :access_key_id     => ENV['S3_KEY'],
-    :secret_access_key => ENV['S3_SECRET']
+    :access_key_id     => 'AKIAIAWWYJYVTOX6NYQQ',
+    :secret_access_key => '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'
   )
 end
