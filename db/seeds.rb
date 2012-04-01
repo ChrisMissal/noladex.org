a16855c8546c79c5c3535e6c393532bdab17d55b
Scott Block <scott@scottblock.com>
2012-04-01 17:38:04 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,14 @@ categories = []
   ["Design", "Develop", "Write", "Illustrate", "Photograph", "Videograph", "Market"].each do | name |
 	categories << Category.create(:name => name)
 end
+
+27.times do |idx|
+  user = User.new(:password => 'secret', :name => "User #{idx + 1}", :email => "user#{idx}@noladex.org", :avatar => File.open(File.join(Rails.root, 'db', 'fixtures', 'kitten.jpeg')))
+  3.times do |mdx|
+    user.missions << Mission.new(:statement => 'mission statements ...', :category_id => rand(5) + 1)
+  end
+  user.url1 = "@user#{idx + 1}"
+  user.url2 = "http://www.user#{idx + 1}.com"
+  user.url3 = "http://wwww.facebook.com/user#{idx + 1}"
+  user.save!
+end
