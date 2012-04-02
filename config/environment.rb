af0d493e38f2eaea5456d27e2dea472d388d6ab5
Scott Block <scott@scottblock.com>
2012-04-01 21:07:19 -0400

    config

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
+config.gem "rmagick",
+:lib => "RMagick" 
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Noladex::Application.initialize!
-
-config.gem "rmagick",
-:lib => "RMagick" 
