fe7a7340ecb8f7fdbb4efb6cf6bdfb942afc14ea
Scott Block <scott@scottblock.com>
2012-04-11 20:00:44 -0400

    bug squashed this time

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def index
     if params[:category] && params.has_key?(:category) && (params[:category].length > 0 )	
-      @users = User.find_by_category(params[:category]).paginate(:per_page => 30, :page => params[:page])
+      @users = User.find_by_category(params[:category]).paginate(:per_page => 100, :page => params[:page])
     else
       @users = User.includes(:missions => :category).order('created_at DESC').paginate(:per_page => 30, :page => params[:page])
     end
